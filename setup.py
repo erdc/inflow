2a013cc73d0ac96a47d2d1050feb895e3ebd7fc4
Matthew <mpg56@drexel.edu>
2022-05-18 15:57:52 -0500

    Adds setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='inflow',
+   version='1.0',
+   description='Tools to accumulate lateral inflow for streamflow modeling.',
+   author='CHL HSB',
+   packages=['inflow'],
+)
